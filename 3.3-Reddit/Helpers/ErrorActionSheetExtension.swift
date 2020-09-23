30e97c27fd34902d68bd86bd799b463c687a054a
Jason Koceja <jason@koceja.com>
2020-09-23 14:42:23 -0700

    added extension

--- 3.3-Reddit/Helpers/ErrorActionSheetExtension.swift
+++ 3.3-Reddit/Helpers/ErrorActionSheetExtension.swift
@@ -1,8 +1,19 @@
 //
-//  File.swift
+//  ErrorActionSheetExtension.swift
 //  3.3-Reddit
 //
 //  Created by Jason Koceja on 9/23/20.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func presentErrorToUser(localizedError: LocalizedError){
+        let alertController = UIAlertController(title: "Error", message: localizedError.errorDescription, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+    
+}
