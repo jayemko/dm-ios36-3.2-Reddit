68d00bb9d12125c1801ff03ba4b06bc92d69c7f4
Jason Koceja <jason@koceja.com>
2020-09-23 14:42:55 -0700

    completed custom cell

--- 3.3-Reddit/Views/PostTableViewCell.swift
+++ 3.3-Reddit/Views/PostTableViewCell.swift
@@ -7,17 +7,44 @@
 
 import UIKit
 
+// MARK: - Protocol
+protocol PresentErrorToUserDelegate: AnyObject {
+    func presentError(error: LocalizedError)
+}
+
 class PostTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var upvoteLabel: UILabel!
+    
+    // MARK: - Helper Functions and Methods
+    
+    // MARK: - Properties
+    var post: Post? {
+        didSet {
+            updateViews()
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    weak var delegate: PresentErrorToUserDelegate?
+    
+    func updateViews() {
+        guard let post = post else { return }
+        titleLabel.text = post.title
+        upvoteLabel.text = "⬆\(post.ups)"
+        thumbnailImageView.image = nil
+        
+        PostController.fetchThumbnailForPost(post: post) { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                    case .success(let image):
+                        self.thumbnailImageView.image = image
+                    case .failure(let error):
+                        self.delegate?.presentError(error: error)
+                        print("Error [\(#function):\(#line)] -- \(error.localizedDescription) \n---\n \(error)")
+                }           
+            }
+        }
     }
-
 }
