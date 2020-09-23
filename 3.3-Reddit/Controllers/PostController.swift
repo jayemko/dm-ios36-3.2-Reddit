b97cd980c5c1edb3f5497f14b8bfe157fb67af4e
Jason Koceja <jason@koceja.com>
2020-09-23 12:54:16 -0700

    added fetch functions

--- 3.3-Reddit/Controllers/PostController.swift
+++ 3.3-Reddit/Controllers/PostController.swift
@@ -5,15 +5,14 @@
 //  Created by Jason Koceja on 9/23/20.
 //
 
-
-// https://www.reddit.com/r/funny/.json
-
 import Foundation
+import UIKit.UIImage
 
 struct StringConstants {
     fileprivate static let baseURL = "https://www.reddit.com"
     fileprivate static let endpointR = "r"
     fileprivate static let endpointFunny = "funny"
+    fileprivate static let extensionJSON = "json"
 }
 
 class PostController {
@@ -22,8 +21,47 @@ class PostController {
         guard let baseURL = URL(string: StringConstants.baseURL) else {return completion(.failure(.invalidURL))}
         let rComponentURL = baseURL.appendingPathComponent(StringConstants.endpointR)
         let funnyComponentURL = rComponentURL.appendingPathComponent(StringConstants.endpointFunny)
+        let finalURL = funnyComponentURL.appendingPathExtension(StringConstants.extensionJSON)
         
         print("\(#function): \(#line) -- \(funnyComponentURL)")
+        
+        URLSession.shared.dataTask(with: finalURL) { (data, _, error) in
+            if let error = error {
+                return completion(.failure(.thrownError(error)))
+            }
+            
+            guard let data = data else { return completion(.failure(.invalidData)) }
+            
+            do {
+                let topLevelObject = try JSONDecoder().decode(TopLevelDictionary.self, from: data)
+                let secondLevelObject = topLevelObject.data
+                let thirdLevelObjects = secondLevelObject.children
+                var posts: [Post] = []
+                for object in thirdLevelObjects {
+                    let post = object.data
+                    posts.append(post)
+                }
+                return completion(.success(posts))
+            } catch {
+                return completion(.failure(.thrownError(error)))
+            }
+        }.resume()
     }
     
+    static func fetchThumbnailForPost(post: Post, completion: @escaping (Result<UIImage, PostError>) -> Void) {
+        guard let thumbnailURL = post.thumbnail else { return completion(.failure(.invalidURL))}
+        print("\(#function): \(#line) -- \(thumbnailURL)")
+        
+        URLSession.shared.dataTask(with: thumbnailURL) { (data, _, error) in
+            if let error = error {
+                return completion(.failure(.thrownImageError(error)))
+            }
+            
+            guard let data = data else { return completion(.failure(.invalidData)) }
+            guard let thumbImage = UIImage(data: data) else {
+                return completion(.failure(.unableToDecode))
+            }
+            return completion(.success(thumbImage))
+        }.resume()
+    }
 }
